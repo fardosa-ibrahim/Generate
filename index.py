6795c2521f9b04a317f517e4986a91b7572d1fa0
fardosa-ibrahim <fardosaibrahimshide@gmail.com>
2022-04-03 12:58:21 +0300

    first commit

---
+++ index.py
@@ -0,0 +1,19 @@
+loop = 1
+while (loop < 10):
+    you= input("Choose a noun: ")
+    notYou = input("Choose a plural noun: ")
+    you2= input("Choose a noun: ")
+    place = input("Name a place: ")
+    adjective = input("Choose an adjective (Describing word): ")
+    you3 = input("Choose a noun: ")
+    print ("------------------------------------------")
+    print ("Be kind to your",you,"- footed", notYou)
+    print ("For a duck may be somebody's", you2,",")
+    print ("Be kind to your",notYou,"in",place)
+    print ("Where the weather is always",adjective,".")
+    print ()
+    print ("You may think that is this the",you3,",")
+    print ("Well it is.")
+    print ("------------------------------------------")
+
+
